5bc70ef8a204aee466fcd686c0c7ad71017f695a
Ansh Singhal <e23cseu0145@bennett.edu.in>
2024-04-03 12:08:55 +0530

    maven project created

---
+++ talent_insider/src/main/java/com/talent_insider/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package com.talent_insider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // JDBC URL, username, and password for connecting to MySQL database
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/talent_insider";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Ansh@123";
+
+    // Method to establish a connection to the database
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+}
